07f71df31837b879e595f1c971e85ee0a4b015d8
SilasHanyecz <silas.hanyecz2@gmail.com>
2026-08-29 16:43:52 +0200

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Home from "./Home/Home.jsx";
+import Navigation from "./Home/Navigation.jsx";
+import Services from "./Home/Services.jsx";
+import Guide from "./Home/Guide.jsx";
+import Booking from "./Home/Booking.jsx";
+import About from "./Home/About.jsx";
+import Footer from "./Home/Footer.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Navigation/>
+        <Home/>
+        <Services/>
+        <Guide/>
+        <Booking/>
+        <About/>
+        <Footer/>
+    </StrictMode>,
 )
